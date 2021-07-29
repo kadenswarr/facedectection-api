16319067b61353cbfd808f53c50592861e3f17b5
kaden <none@none.com>
2021-07-28 21:48:06 -0600

    database updates

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt-nodejs');
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   }
 })
